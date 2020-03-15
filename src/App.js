ccf5a12646012d16f00a92ccb2a702372d8a1738
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-03-15 18:57:54 +0000

    first

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './compornent/navbar';
+class App extends Component {
+  render(){
+  return <Navbar />;
+  }
+}
+
+export default App;
---
+++ src/compornent/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+function navbar(){
+    return(
+        <div>
+            <nav class="navbar navbar-light bg-dark mb-5">
+                <div class="container">
+                    <div class="navbar-header">
+                        <a class="navbar-brand text-white text-lg brand-text" href="#">
+                            MoviePort
+                        </a>
+                    </div>
+                </div>
+
+            </nav>
+        </div>
+    );
+}
+export default navbar;
