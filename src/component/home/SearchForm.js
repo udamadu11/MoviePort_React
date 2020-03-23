f9799371107fe3aa2b2b86003fc98cc6b326d9ca
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-03-23 17:31:56 +0000

    0.5

--- src/component/home/SearchForm.js
+++ src/component/home/SearchForm.js
@@ -5,7 +5,7 @@ export class SearchForm extends Component{
             <div className="jumbotron jumbotron-fluid mt-5 text-center">
                 <div className="container">
                     <h1 className="display-4 mb-3">
-                        <i className="fa fa-search" />Search For Movie, Tv series...
+                        <i className="fa fa-search" />Search For Movie, Tv series
                     </h1>
                     <form id=""searchForm>
                         <input 
