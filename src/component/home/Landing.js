0fd335b8f2cdcdcea846fb8adf26106eea4c9df8
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-04-03 16:01:41 +0100

    0.7

--- src/component/home/Landing.js
+++ src/component/home/Landing.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import SearchForm from './SearchForm';
+import {connect} from 'react-redux';
+import Spinner from '../layout/Spinner';
+import  MoviesContainer from './MoviesContainer';
 
 export class Landing extends Component{
     render(){
+        const {loading} = this.props;
         return(
             <div className="container">
                 <SearchForm />
+                {loading ? <Spinner/> : <MoviesContainer />}
             </div>
         );
     }
 }
-export default Landing;
+const mapStateToProps = state => ({
+    loading: state.movies.loading
+});
+export default connect(mapStateToProps)(Landing);
---
+++ src/component/home/MovieCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+export class MovieCard extends Component{
+   render(){
+       const {movie} = this.props;
+    return(
+        <div>
+           <div className="col-md-3 mb-5">
+                <div className="card card-body bg-dark text-center h-100">
+                    <img className="w-100 mb-2" src={movie.Poster} alt= "Movie Cover" />
+                    <h5 className="text-light card-title">
+                        {movie.Title} - {movie.Year}
+                    </h5>
+                    <a className="btn btn-primary" href="#">
+                        Movie Details
+                    </a>
+                    <i className="fas fa-chevron-right"></i>
+                </div>
+           </div>
+        </div>
+    )
+   } 
+}
+export default MovieCard;
